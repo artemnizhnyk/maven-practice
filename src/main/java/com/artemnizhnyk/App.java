5175adf1e91f40bc0625be05d4a4c596f5656319
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-10-07 18:32:32 +0200

    Initial commit

---
+++ src/main/java/com/artemnizhnyk/App.java
@@ -0,0 +1,12 @@
+package com.artemnizhnyk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
