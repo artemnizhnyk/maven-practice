fc32cd2739962b5c4f1e660500850bf42d871bcd
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-10-30 12:11:58 +0100

    practice with failsafe plugin

---
+++ src/test/java/com/artemnizhnyk/integration/ExampleIT.java
@@ -0,0 +1,13 @@
+package com.artemnizhnyk.integration;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ExampleIT {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
